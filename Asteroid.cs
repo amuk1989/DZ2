60012eab1d9487465ad112206bf1df9224189d37
amuk1989 <58622904+amuk1989@users.noreply.github.com>
2020-02-29 02:02:37 +0700

    Objects

---
+++ Asteroid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+    class Asteroid : BaseObject
+    {
+        Image fileOfStar = Image.FromFile("star1.png");
+    }
+}
